9ee779e41d7904da3324e6d263da6e3a6813f530
aibek <ozhorov@gmail.com>
2020-06-14 23:06:43 -0400

    working on event listeners

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   password = '';
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+
+  onChangeUseLetters(){
+    this.includeLetters = !this.includeLetters
+  }
+
+  onChangeUseSymbols (){
+    this.includeSymbols = !this.includeSymbols
+  }
+  onChangeUseNumbers (){
+    this.includeNumbers = !this.includeNumbers
+  }
+  // constructor (){
+  //   this.password = '';
+  // }
+  
  onButtonClick(){
+   console.log(this.includeLetters)
+   console.log(this.includeSymbols)
+   console.log(this.includeNumbers)
+
    this.password = 'My Password'
  }
 }
